3ab798bca5687fbe78ed5d32f6b16b118d7f756c
EmaCartoon <emacartoon@gmail.com>
2021-05-09 11:33:27 -0700

    Updated Project list and Added In Progress section

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Container from "../components/container/container";
 import Card from "../components/card/card";
 import projects from "../components/card/projects.json";
+import working from "../components/card/working.json";
 
 class Work extends Component {
   state = { projects };
@@ -24,6 +25,19 @@ class Work extends Component {
                   name={project.name}
                   img={project.img}
                   link={project.link}
+                />
+              ))}
+            </div>
+          </div>
+          <h2>Projects I'm Currently Working On</h2>
+          <div>
+            <div className="flex-row-evenly">
+              {this.state.working.map((work) => (
+                <Card
+                  key={work.name}
+                  name={work.name}
+                  img={work.img}
+                  link={work.link}
                 />
               ))}
             </div>
